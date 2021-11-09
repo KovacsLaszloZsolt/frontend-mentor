e3d3bd8cdd9225737457e073eefcdd93a072dfab
Kovacs Laszlo Zsolt <laszlo.zs.kovacs@gmail.com>
2021-11-09 05:18:14 +0100

    feat: add filter event handle functions

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,13 +2,42 @@ import './App.css';
 import Header from '../Header/Header';
 import SearchBar from '../SearchBar/SearchBar';
 import Filter from '../Filter/Filter';
+import CountryCardsCtn from '../CountryCardsCtn/CountryCardsCtn';
+import { useEffect, useState } from 'react';
+import data from '../../data/data';
 
 function App() {
+  const URL = "https://restcountries.com/v3.1/all";
+  const [counrtiesDetails, setCountriesDetails] = useState(null);
+
+  useEffect(() => {
+    setCountriesDetails(data);
+    setQueryData(data);
+    //     fetch(URL)
+    //         .then(response => response.json())
+    //         .then((data) => {
+    //             setCountriesDetails(data);
+    //         })
+  }, []);
+  const [filter, setFilter] = useState('');
+  const [search, setSearch] = useState('');
+  const [queryData, setQueryData] = useState('');
+
+  useEffect(() => {
+    if (counrtiesDetails) {
+      setQueryData(counrtiesDetails
+        .filter(country => country.name.common.toLowerCase()
+          .includes(search.toLowerCase())).filter(country => country.region.includes(filter)));
+    }
+  }, [search, filter]);
+
   return (
     <div className="App">
       <Header />
-      <SearchBar />
-      <Filter />
+      <SearchBar setSearch={setSearch} />
+      <Filter filter={filter} setFilter={setFilter} />
+      <CountryCardsCtn counrtiesDetails={queryData} />
+
     </div>
   );
 }
--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,17 +1,64 @@
+import { useState } from 'react';
 import './Filter.css';
 
-const Filter = () => {
-    const handleFilterOnClick = (e) => {
-        console.log(e.target);
+const Filter = ({ filter, setFilter }) => {
+    const [isFilterOpen, setIsFilterOpen] = useState(false)
+    const handleFilterDisplay = () => {
+
+        const currentFilterOpen = !isFilterOpen;
+
+        const arrow = document.querySelector('.arrow-icon');
+        const filter = document.querySelector('.filter');
+        const filterTitle = document.querySelector('.filter-base-title');
+
+        if (currentFilterOpen) {
+            arrow.innerText = 'expand_more';
+            filter.classList.remove('filter-inactive');
+
+            if (filterTitle.innerText !== 'Filter by Region') {
+                filterTitle.innerText = 'Filter by Region';
+            }
+
+        } else {
+            arrow.innerText = 'expand_less';
+            filter.classList.add('filter-inactive');
+        }
+
+        setIsFilterOpen(currentFilterOpen);
     }
-    const continents = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+
+    const handleFilterOnClick = (e) => {
+
+        const arrow = document.querySelector('.arrow-icon');
+        const filter = document.querySelector('.filter');
+        const filterTitle = document.querySelector('.filter-base-title');
+
+        arrow.innerText = 'expand_less';
+        filter.classList.add('filter-inactive');
+        filterTitle.innerText = e.target.innerText;
+
+        setFilter(e.target.innerText === 'All' ? '' : e.target.innerText);
+        setIsFilterOpen(false);
+    }
+
+    const continents = ['All', 'Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+
     return (
-        <div className="select-ctn">
-            <ul className="filter-elems-ctn">
-                <li className="filter-elem filter-elem-selected" onClick={handleFilterOnClick}>Filter by Region</li>
-                <li className="filter-elem inactive">Africa</li>
-                <li className="filter-elem inactive">America</li>
-                <li className="filter-elem inactive">Europa</li>
+        <div className="filter-ctn">
+            <div className="filter-base" onClick={handleFilterDisplay}>
+                <span className="filter-base-title">Filter by Region</span>
+                <span className="arrow-icon material-icons-outlined">
+                    expand_less
+                </span>
+            </div>
+            <ul className="filter filter-inactive">
+                {continents.map(continent => (
+                    <li key={continent}
+                        className="filter-elem inactive-filter"
+                        onClick={handleFilterOnClick}>
+                        {continent}
+                    </li>
+                ))}
             </ul>
         </div >
     );
