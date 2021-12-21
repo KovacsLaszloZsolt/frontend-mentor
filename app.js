3e99240573650d80bd75bf3c5d45ae6ed814dc74
Kovacs Laszlo Zsolt <laszlo.zs.kovacs@gmail.com>
2021-12-21 10:25:06 +0000

    add app.js

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+const header = document.querySelector("header");
+
 const menuIcon = document.querySelector(".menu-icon");
 const navMenu = document.querySelector(".nav-menu");
+const main = document.querySelector("main");
 
 menuIcon.addEventListener("click", () => {
   navMenu.classList.toggle("menu-active");
@@ -8,3 +11,10 @@ menuIcon.addEventListener("click", () => {
     ? menuIcon.setAttribute("src", "./assets/shared/icon-close.svg")
     : menuIcon.setAttribute("src", "./assets/shared/icon-hamburger.svg");
 });
+
+
+
+// const app = new App();
+// app.setNavMenu();
+// app.setHomeModal();
+// app.setPages();
